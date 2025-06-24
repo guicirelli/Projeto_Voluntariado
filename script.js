3148e71056fa98214de7cb0faf287b8ea404e96b
Guicirelli <guilopes.030206@gmail.com>
2025-06-24 19:29:48 -0300

    js: Define variáveis globais e elementos do DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+// ===================== Variáveis globais =====================
+
+const sections = document.querySelectorAll('.secao');
+const navButtons = document.querySelectorAll('.nav-btn');
+
+const form = document.getElementById('formNecessidade');
+const listaNecessidades = document.getElementById('listaNecessidades');
+const filtroBusca = document.getElementById('filtroBusca');
+const filtroTipo = document.getElementById('filtroTipo');
+const msgSucesso = document.querySelector('.msg-sucesso');
+
+let necessidades = [];
